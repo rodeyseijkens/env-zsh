03bc9d61e00cea2676786b6a5b992ced2a29e48c
Rodey <me@rodey.nl>
2021-05-22 16:34:34 +0200

    Update env.plugin.zsh

--- env.plugin.zsh
+++ env.plugin.zsh
@@ -3,8 +3,11 @@
 autoload -U add-zsh-hook
 load-local-conf() {
      # check file exists, is regular file and is readable:
-     if [[ -f .env && -r .env ]]; then
-       source .env
+     if [[ -f .env.local && -r .env.local ]]; then
+       set -a
+       source <(cat .env.local | sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g")
+       set +a
+       echo "Loaded variables from .env.local"
      fi
 }
 add-zsh-hook chpwd load-local-conf
